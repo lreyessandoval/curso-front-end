8a3c417c5d47cc0144b986638f3b7500ead4696a
Luis Reyes Sandoval <lreyessandoval@gmail.com>
2020-02-20 19:22:23 -0300

    jscript send contact to backend...

--- portafolio/assets/js/script.js
+++ portafolio/assets/js/script.js
@@ -1,16 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    addsmoothScrolling();
+    addsendContact();
 
-    $("a").click(function(event) {
+});
+
+function addsendContact() {
+
+    $("#btn-sendcontact").click(function () {
+        sendEmail();
+    });
+
+}
+
+function addsmoothScrolling() {
+    $("a").click(function (event) {
         if (this.hash !== "") {
             event.preventDefault();
             var gato = this.hash;
             $('html, body').animate({
                 scrollTop: $(gato).offset().top
-            }, 800, function() {
+            }, 800, function () {
                 window.location.hash = gato;
             });
         }
     });
+}
 
+function sendEmail() {
+    var contact = {
+        name: $("#fld-name").val(),
+        email: $("#fld-email").val(),
+        phone: $("#fld-phone").val(),
+        comment: $("#fld-comment").val(),
+    }
 
-});
+    alert(JSON.stringify(contact));
+}
