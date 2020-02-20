578b7108084bf0fef7501a219616e4e6ef5c454f
Luis Reyes Sandoval <lreyessandoval@gmail.com>
2020-02-20 20:15:02 -0300

    Add validation to FormContact...

--- portafolio/assets/js/script.js
+++ portafolio/assets/js/script.js
@@ -12,6 +12,38 @@ function addsendContact() {
 
 }
 
+function sendEmail() {
+    var contact = {
+        name: $("#fld-name").val(),
+        email: $("#fld-email").val(),
+        phone: $("#fld-phone").val(),
+        comment: $("#fld-comment").val(),
+    }
+    var message = "";
+
+    if (contact.name == "") message += "Favor ingresar el Nombre...<br>"
+    if (contact.email == "") message += "Favor ingresar el E-Mail...<br>"
+    if (contact.phone == "") message += "Favor ingresar el Teléfono...<br>"
+    if (contact.comment == "") message += "Favor ingresar el Comentario...<br>"
+
+    var link = $("a[href='#Contact']");;
+    if (message == "") {
+        clearForm();
+        message = "Gracias por completar el formulario... en breve nos pondremos en contacto...";
+        link = $("a[href='#Home']");
+    }
+    link.click();
+    $('#exampleModalCenter div .modal-body').html(message);
+    $('#exampleModalCenter').modal();
+
+    //alert(JSON.stringify(contact));
+}
+function clearForm() {
+    $("#fld-name").val('');
+    $("#fld-email").val('');
+    $("#fld-phone").val('');
+    $("#fld-comment").val('');
+}
 function addsmoothScrolling() {
     $("a").click(function (event) {
         if (this.hash !== "") {
@@ -24,15 +56,4 @@ function addsmoothScrolling() {
             });
         }
     });
-}
-
-function sendEmail() {
-    var contact = {
-        name: $("#fld-name").val(),
-        email: $("#fld-email").val(),
-        phone: $("#fld-phone").val(),
-        comment: $("#fld-comment").val(),
-    }
-
-    alert(JSON.stringify(contact));
 }
