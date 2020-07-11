9cc88d193c653f1f37e549d879b4c36d397e1592
Luis Reyes Sandoval <lreyessandoval@gmail.com>
2020-07-10 22:40:12 -0400

    Ajuste formato ES6...

--- modulo.03.02/node-mvc-2/controllers/users.js
+++ modulo.03.02/node-mvc-2/controllers/users.js
@@ -33,6 +33,41 @@ exports.getUserList = function(req, res, next) {
 }
 
 
-exports.lista_usuarios = function() {
-    return User.find({});
+exports.lista_usuarios = async(req, res, next) => {
+
+    var data = await User.find({});
+    /* Replace Data */
+    data = data.map(item => {
+        return {
+            "name": reemplazar_nombres(item.name),
+            "lastname": reemplazar_nombres(item.lastname),
+            "email": item.email,
+        };
+    });
+
+    /* Sort Data */
+    data = sortByKeyAsc(data, 'name');
+
+    res.render('newuser', { title: 'Nuevo Usuario', records: data });
+}
+
+
+const reemplazar_nombres = function(value) {
+    return value.replace("ñ", "nn").replace("Ñ", "NN");
+}
+
+const sortByKeyDesc = function(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key];
+        var y = b[key];
+        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+    });
+}
+
+const sortByKeyAsc = function(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key].toLowerCase();
+        var y = b[key].toLowerCase();
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
 }
